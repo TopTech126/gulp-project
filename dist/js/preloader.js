7a3fce87a721a0e1defcbacfdcfbc8c6bdff60c8
toptech126 <carlie197821@gmail.com>
2024-12-20 11:26:05 +0200

    update

--- dist/js/preloader.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function () {
-// document.addEventListener('DOMContentLoaded', function () {
-  // When the content is loaded, add the 'loaded' class to the body
-  document.body.classList.add('loaded');
-});
